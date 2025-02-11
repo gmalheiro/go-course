ac42192c6ceccae5c8f637d65c0f58dc0bc0be6a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-02-11 01:29:36 -0300

    test: add transfer test with CreateTransfer test

---
+++ simplebank/db/sqlc/transfer_test.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/gmalheirog/go-course/util"
+)
+
+func TestCreateTransfer(t *testing.T) Transfer {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+	require.Equal(t, transfer.Amount, arg.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+	return transfer
+}
